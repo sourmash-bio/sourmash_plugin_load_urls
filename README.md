32b7f838922577e73b5bcaf032783383b7e93a62
C. Titus Brown <titus@idyll.org>
2023-01-01 08:54:12 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# sourmash plugin to load files from URLs with fsspec.
+
+This is an **experimental** plugin to support loading signature files
+from URLs with fsspec.
+
+Links:
+* [sourmash: sourmash-bio/sourmash/](https://github.com/sourmash-bio/sourmash/)
+* [fsspec](https://filesystem-spec.readthedocs.io/en/latest/index.html)
+
+Note: Depends on
+[sourmash#2428](https://github.com/sourmash-bio/sourmash/pull/2428).
+
+## Usage
+
+```
+pip install .
+```
+
+and then you can go on your merry way to load URLs!
---
+++ src/sourmash_plugin_fsspec.py
@@ -0,0 +1,40 @@
+"Read sketches with fsspec."
+import sourmash
+import fsspec
+from sourmash.index import LinearIndex, MultiIndex, StandaloneManifestIndex
+from sourmash.manifest import BaseCollectionManifest
+import traceback
+
+# CTB: support file lists, CSV files?
+
+###
+
+# @CTB: figure out parent/prefix stuff?
+
+def load_sketches(location, *args, **kwargs):
+    of = fsspec.open(location, 'rb')
+
+    try:
+        with of as fp:
+            # load JSON if we can -
+            lidx = LinearIndex.load(fp, filename=location)
+
+            # and then pass into MultiIndex to generate a manifest.
+            db = MultiIndex.load((lidx,), (None,), parent=None)
+
+            return db
+    except sourmash.exceptions.SerdeError:
+        pass
+
+    # if we can't read it as a JSON signature, try it as a manifest.
+    # CTB: not sure why we can't keep it open in the above block and
+    # then do a seek here? But anyway...
+
+    of = fsspec.open(location, 'rt')
+    with of as fp:
+        mf = BaseCollectionManifest.load_from_csv(fp)
+        return StandaloneManifestIndex(mf, location, prefix='')
+
+
+# default priority is ok here
+#load_sketches.priority = 5
